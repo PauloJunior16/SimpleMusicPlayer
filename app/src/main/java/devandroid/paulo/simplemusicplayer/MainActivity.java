c39ce2abee133719d5566112879c639a82024ece
Paulo Junior <pcjsouza16@gmail.com>
2023-04-26 22:45:50 -0300

    Created song list fragment, adapter to retrieve musics from local storage, and layout to list the songs

--- app/src/main/java/devandroid/paulo/simplemusicplayer/MainActivity.java
+++ app/src/main/java/devandroid/paulo/simplemusicplayer/MainActivity.java
@@ -14,9 +14,14 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
@@ -27,7 +32,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 1;
-
+    static ArrayList<MusicFiles> musicFiles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
         permission();
 
-        initViewPager();
     }
 
     private void permission() {
@@ -43,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}
                     , REQUEST_CODE);
         } else {
-            Toast.makeText(this, "Permission Granted!", Toast.LENGTH_LONG).show();
+            musicFiles = getAllAudio(this);
+            initViewPager();
         }
     }
 
@@ -53,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_CODE) {
             {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission Granted!", Toast.LENGTH_LONG).show();
+                    musicFiles = getAllAudio(this);
+                    initViewPager();
 
                 } else {
                     ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}
@@ -110,5 +116,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public static ArrayList<MusicFiles> getAllAudio(Context context)
+    {
+        ArrayList<MusicFiles> tempAudioList = new ArrayList<>();
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.ARTIST
+        };
+        Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
 
+        if (cursor != null) {
+            while (cursor.moveToNext())
+            {
+                String album = cursor.getString(0);
+                String title = cursor.getString(1);
+                String duration = cursor.getString(2);
+                String path = cursor.getString(3);
+                String artist = cursor.getString(4);
+
+                MusicFiles musicFiles = new MusicFiles(path, title, artist, album, duration);
+
+//                Log.e("Path : " + path, "Album : " + album);
+                tempAudioList.add(musicFiles);
+            }
+            cursor.close();
+        }
+        return tempAudioList;
+    }
 }
